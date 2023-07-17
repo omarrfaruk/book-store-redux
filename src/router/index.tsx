37d21ae23f81d6cac9e88f5049ca14a953adcfa9
omar faruk~ <avirus170@gmail.com>
2023-07-17 20:49:14 +0600

    added router outlet

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,44 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App';
+import Login from "../page/Login";
+import Signup from "../page/Signup";
+import Home from "../page/Home";
+import Books from "../page/Books";
+import EditBook from "../page/EditBook";
+import AddBook from "../page/AddBook";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
     children:[
-      
+      {
+        index:true,
+        element:<Home/>
+      },
+      {
+        path:'/books',
+        element:<Books/>
+      },{
+        path:'/editbook/:id',
+        element:<EditBook/>
+      },{
+        path:'/bookdetails/:id',
+        element:<EditBook/>
+      },{
+        path:'/addbook',
+        element:<AddBook/>
+      }
     ]
   },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/signup",
+    element:<Signup/>
+  }
 ]);
 
 export default router;
